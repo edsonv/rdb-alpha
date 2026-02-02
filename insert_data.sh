e4f7404828b7688656a2a08cfc623ddedb8b0e84
Edson J Vargas <edson.jhair@yahoo.com>
2026-02-01 23:55:41 -0500

    Day 1

--- insert_data.sh
+++ insert_data.sh
@@ -8,3 +8,26 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+TEAMS=$(
+  cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOAL OPPONENT_GOALS;
+  do
+    if [[ $YEAR != "year" ]]
+    then
+      echo -e "$WINNER\n$OPPONENT"
+    fi
+  done | sort | uniq | sed "s/'/''/g"
+)
+
+VALUES=$(echo "$TEAMS" | sed "s/^/('/; s/$/')/" | sed ':a;N;$!ba;s/\n/,/g')
+
+$PSQL "INSERT INTO teams(name) VALUES $VALUES ON CONFLICT (name) DO NOTHING;"
+
+GAMES=$(
+  cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOAL OPPONENT_GOALS;
+  do
+    if [[ $YEAR != "year" ]]
+    then
+      echo -e "$WINNER\n$OPPONENT"
+    fi
+  done | sort | uniq | sed "s/'/''/g"
+)
